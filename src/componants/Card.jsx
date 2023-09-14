400c726af6834f45ef0a5020e641d54211e2c6ab
Rohit Kumar Modi <rohitarya7478@gmail.com>
2023-09-14 20:59:34 +0530

    new changes

--- src/componants/Card.jsx
+++ src/componants/Card.jsx
@@ -35,17 +35,18 @@ const Card = () => {
      
        return ( 
          <Link to={`/detail/${e.id}`}> 
-           <div key={i} className='card font-medium shadow-lg rounded-md p-2 m-2 hover:-translate-y-3 cursor-pointer mt-3 transition-all duration-500 '>
-            <img className=' h-40 w-36 md:h-60 md:w-56  ' src={e.image} alt="" />
-            <h1 className=' text-center md:text-lg '> <span className=' text-gray-500   '></span> {e.title}</h1>
-            <h1 className='flex items-center '> <span className=' text-gray-500  mr-1'>Rating: </span><ReactStars
-              size={20}
-              half={true}
-              value={e.rating / e.rated}
-              edit={false}
-            />
-            </h1>
-            <h1> <span className=' text-gray-500 mr-1 '>Year:  </span> {e.year} </h1>
+     
+           <div key={i} className='card font-medium shadow-lg rounded-md p-1 m-2 hover:-translate-y-3 cursor-pointer mt-3 transition-all duration-500 '>
+              <img className=' h-40 w-36 md:h-60 md:w-48  ' src={e.image} alt="" />
+              <h1 className=' text-center md:text-lg '> <span className=' text-gray-500   '></span> {e.title}</h1>
+              <h1 className='flex items-center '> <span className=' text-gray-500  mr-1'>Rating: </span><ReactStars
+                size={20}
+                half={true}
+                value={e.rating / e.rated}
+                edit={false}
+              />
+              </h1>
+              <h1> <span className=' text-gray-500 mr-1 '>Year:  </span> {e.year} </h1>
           </div>
         </Link>
 
